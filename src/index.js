849e4cd70c2298548db436a654cff45fb53985f9
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-08-26 16:02:09 +0300

    smooth scroll added

--- src/index.js
+++ src/index.js
@@ -84,5 +84,14 @@ function onLoadMore() {
       loadMoreHide();
       finalText.classList.replace('final-text-hidden', 'final-text');
     }
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
   });
 }
